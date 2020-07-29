3843bec05a390b7ffb7002a55f29cf5e4629e29b
Sad Eamin <sadeamin13@gmail.com>
2020-07-29 15:21:29 +0600

    changing something

--- manage_menu_window.py
+++ manage_menu_window.py
@@ -3,11 +3,12 @@ from tkinter import Toplevel, ttk, Text
 from connection_poll import connection_pool
 from tkinter import messagebox
 from psycopg2 import errors
+from tkinter import *
 
 def Manage_menu_window():
     top = Toplevel()
     top.title("Show Menu")
-    top.geometry("1301x800+250+130")
+    top.geometry("1301x820+250+130")
     top.resizable(False, False)
 
     main_heading = ttk.Label(top, text="Restaurant Management System", foreground="red",
@@ -30,26 +31,31 @@ def Manage_menu_window():
         top_1.geometry("620x495+550+230")
         top_1.resizable(False, False)
 
-
-
         label_id = ttk.Label(top_1,
                              text="Menu Item Id :                                                                        ", font=("arial", 15))
         label_id.pack(pady=5)
-        entry_id = Text(top_1, width=50, height=1, font=("arial", 15))
+
+        strvar1 = StringVar()
+
+        entry_id = ttk.Entry(top_1, width=50, font=("arial", 15), textvariable=strvar1)
         entry_id.pack(pady=20)
 
         label_id = ttk.Label(top_1,
                              text="Menu Item Name :                                                                  ", font=("arial", 15))
         label_id.pack(pady=5)
 
-        entry_name = Text(top_1, width=50, height=1, font=("arial", 15))
+        strvar2 = StringVar()
+
+        entry_name = ttk.Entry(top_1, width=50, font=("arial", 15), textvariable=strvar2)
         entry_name.pack(pady=20)
 
         label_id = ttk.Label(top_1,
                              text="Menu Item Price :                                                                   ", font=("arial", 15))
         label_id.pack(pady=5)
 
-        entry_price = Text(top_1, width=50, height=1, font=("arial", 15))
+        strvar3 = StringVar()
+
+        entry_price = ttk.Entry(top_1, width=50, font=("arial", 15), textvariable=strvar3)
         entry_price.pack(pady=20)
 
 
@@ -70,9 +76,9 @@ def Manage_menu_window():
 
         def Save():
 
-            if len(entry_id.get(0.0, "end")) == 0 or \
-                    len(entry_name.get(0.0, "end")) == 0 or \
-                    len(entry_price.get(0.0, "end")) == 0 or len(combobox_type.get()) == 0:
+            if len(entry_id.get()) == 0 or \
+                    len(entry_name.get()) == 0 or \
+                    len(entry_price.get()) == 0 or len(combobox_type.get()) == 0:
                 messagebox.showerror("warning", "Id id empty please fill it.")
 
             else:
@@ -80,45 +86,42 @@ def Manage_menu_window():
                     with connection.cursor() as cursor:
                         cursor.execute("insert into menu(id, name, price, type) "
                                        "values(%s, %s, %s, %s)",
-                                       (entry_id.get(0.0, "end"), entry_name.get(0.0, "end"),
-                                        entry_price.get(0.0, "end"), combobox_type.get()))
+                                       (entry_id.get(), entry_name.get(),
+                                        entry_price.get(), combobox_type.get()))
 
                         connection.commit()
                         cursor.close()
                         messagebox.showinfo(title="success", message="successfully")
 
         def New():
-            entry_id.delete(0.0, "end")
-            entry_name.delete(0.0, "end")
-            entry_price.delete(0.0, "end")
+            entry_id.delete(0, "end")
+            entry_name.delete(0, "end")
+            entry_price.delete(0, "end")
             combobox_type.set("")
 
         def cancel():
             top_1.destroy()
 
+        button_save = Button(frame_buttons_1, text="Save", command=Save, bg="blue", fg="#FFD5C6", height=3, width=15)
 
-        button_save = ttk.Button(frame_buttons_1, text="Save", command=Save)
+        button_new = Button(frame_buttons_1, text="New", command=New, bg="blue", fg="#FFD5C6", height=3, width=15)
 
-        button_new = ttk.Button(frame_buttons_1, text="New", command=New)
-        button_cancel = ttk.Button(frame_buttons_1, text="Cancel", command=cancel)
+        button_cancel = Button(frame_buttons_1, text="Cancel", command=cancel, bg="blue", fg="#FFD5C6", height=3, width=15)
 
-        button_save.grid(row=0, column=0, padx=5, ipadx=50, ipady=30)
-        button_new.grid(row=0, column=1, padx=5, ipadx=50, ipady=30)
-        button_cancel.grid(row=0, column=2, padx=5, ipadx=50, ipady=30)
-
-
+        button_save.grid(row=0, column=0, padx=5, ipadx=10)
+        button_new.grid(row=0, column=1, padx=5, ipadx=10)
+        button_cancel.grid(row=0, column=2, padx=5, ipadx=10)
 
 
-
-    button_add_item = ttk.Button(frame_buttons, text="Add New menu item", command=Add_new_item)
+    button_add_item = Button(frame_buttons, text="Add New menu item", command=Add_new_item, bg="blue", fg="white")
     button_add_item.grid(ipadx=30, ipady=20, padx=10)
 
-    button_edit_item = ttk.Button(frame_buttons, text="Edit New menu item")
+    button_edit_item = Button(frame_buttons, text="Edit New menu item", bg="blue", fg="white")
     button_edit_item.grid(ipadx=30, ipady=20, row=0, column=1, padx=10)
 
-    button_delete_item = ttk.Button(frame_buttons, text="Delete New menu item")
+    button_delete_item = Button(frame_buttons, text="Delete New menu item", bg="blue", fg="white")
     button_delete_item.grid(ipadx=30, ipady=20, row=0, column=2, padx=10)
 
-    button_refresh = ttk.Button(frame_buttons, text="Refresh")
+    button_refresh = Button(frame_buttons, text="   Refresh    ", bg="blue", fg="white")
     button_refresh.grid(ipadx=30, ipady=20, row=0, column=3, padx=10)
 
